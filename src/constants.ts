8e637ef8211b8290e91edc155c387ac7afebe322
Vytas <cepulis.vytas@gmail.com>
2024-04-28 19:09:37 +0300

    added new animations

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,6 @@ export const animations = [
   ['animate__fadeInUp', 'animate__fadeOutUp'],
   ['animate__flipInX', 'animate__flipOutX'],
   ['animate__rollIn', 'animate__rollOut'],
+  ['animate__bounceInLeft', 'animate__bounceOutRight'],
+  ['animate__bounceIn', 'animate__bounceOut'],
 ];
